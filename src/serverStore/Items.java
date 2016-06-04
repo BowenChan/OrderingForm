43db84ac7a3b3ca0234b0f16c20ca83d9491dab8
Bowen Chan <Bowenmchan@gmail.com>
2016-06-04 09:35:21 -0700

    Fixed hibernate config

--- src/serverStore/Items.java
+++ src/serverStore/Items.java
@@ -1,6 +1,8 @@
 package serverStore;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -9,4 +11,13 @@ public class Items {
 	
 	private int JasmineTea;
 	
+	@Id
+	@GeneratedValue
+	private int getJasmineTea(){
+		return JasmineTea;
+	}
+	
+	private void setJasmineTea(int stock){
+		JasmineTea = stock;
+	}
 }
